aa57520df750fbd9850556f98b178d00a90d98bc
Rendy Saputra <rendy@live.com>
2026-03-23 19:10:15 +1100

    feat: show the list

--- 15-ToDoListAppWithVite/project/todo-app/src/App.tsx
+++ 15-ToDoListAppWithVite/project/todo-app/src/App.tsx
@@ -1,9 +1,26 @@
 import "./App.css";
 import TodoForm from "./components/TodoForm";
+import { useState } from "react";
+
 function App() {
+  const [todos, setTodos] = useState<string[]>([]);
+
+  const addTodo = (todo: string) => {
+    setTodos((prev: string[]) => [...prev, todo]);
+  };
+
   return (
     <div>
-      <TodoForm />
+      <TodoForm onAdd={addTodo} />
+      {todos.length > 0 ? (
+        <ul>
+          {todos.map((todo, index) => (
+            <li key={index}>{todo}</li>
+          ))}
+        </ul>
+      ) : (
+        <p>No task yet</p>
+      )}
     </div>
   );
 }
