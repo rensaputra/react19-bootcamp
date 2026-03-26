6437f4a628116b31bbebbf89cac64156b7b79055
Rendy Saputra <1814138+rensaputra@users.noreply.github.com>
2026-03-26 21:33:29 +1100

    feat: sidebar styling

--- 20-MyStore-AdminSection/project/admin/src/app/components/Sidebar/index.tsx
+++ 20-MyStore-AdminSection/project/admin/src/app/components/Sidebar/index.tsx
@@ -1,52 +1,61 @@
 import Link from "next/link";
 import Image from "next/image";
+import { HomeIcon, UsersIcon, SwatchIcon, ShoppingIcon } from "../icons";
 
 export default function Sidebar() {
   const menuItems = [
     {
       text: "Dashboard",
       url: "/dashboard",
+      icon: <HomeIcon />,
     },
     {
       text: "Users",
       url: "/users",
+      icon: <UsersIcon />,
     },
     {
       text: "Product Type",
       url: "/product-type",
+      icon: <SwatchIcon />,
     },
     {
       text: "Products",
       url: "/products",
+      icon: <ShoppingIcon />,
     },
   ];
 
   return (
     <div className="sidebar-main">
-      <div>
-        <h1>eStore</h1>
+      <div className="p-4 m-4">
+        <h1 className="text-3xl font-semibold">eStore</h1>
       </div>
-      <ul>
+      <ul className="mx-auto text-lg flex flex-col">
         {menuItems.map((item) => (
           <li key={item.text}>
             <Link href={item.url}>
-              <div>{item.text}</div>
+              <div className="sidebar-list-item">
+                <span className="mx-2">{item.icon}</span>
+                {item.text}
+              </div>
             </Link>
           </li>
         ))}
       </ul>
 
-      <div>
-        <div>
+      <div className="sidebar-usercard">
+        <div className="flex flex-row m-5 mb-8">
           <Image
             height={50}
             width={50}
             src="./user.svg"
             alt="User"
             style={{ borderRadius: "50%" }}
+            className="border-gray-600 rounded-full border-2"
           ></Image>
+          <div className="m-auto text-lg"> Rendy Saputra </div>
         </div>
-        <div> Rendy Saputra </div>
       </div>
     </div>
   );
---
+++ 20-MyStore-AdminSection/project/admin/src/app/components/icons.jsx
@@ -0,0 +1,67 @@
+export const HomeIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    strokeWidth={1.5}
+    stroke="currentColor"
+    className="size-6"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+    />
+  </svg>
+);
+
+export const UsersIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    strokeWidth={1.5}
+    stroke="currentColor"
+    className="size-6"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M15 19.128a9.38 9.38 0 0 0 2.625.372 9.337 9.337 0 0 0 4.121-.952 4.125 4.125 0 0 0-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 0 1 8.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0 1 11.964-3.07M12 6.375a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0Zm8.25 2.25a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z"
+    />
+  </svg>
+);
+
+export const SwatchIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    strokeWidth={1.5}
+    stroke="currentColor"
+    className="size-6"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M4.098 19.902a3.75 3.75 0 0 0 5.304 0l6.401-6.402M6.75 21A3.75 3.75 0 0 1 3 17.25V4.125C3 3.504 3.504 3 4.125 3h5.25c.621 0 1.125.504 1.125 1.125v4.072M6.75 21a3.75 3.75 0 0 0 3.75-3.75V8.197M6.75 21h13.125c.621 0 1.125-.504 1.125-1.125v-5.25c0-.621-.504-1.125-1.125-1.125h-4.072M10.5 8.197l2.88-2.88c.438-.439 1.15-.439 1.59 0l3.712 3.713c.44.44.44 1.152 0 1.59l-2.879 2.88M6.75 17.25h.008v.008H6.75v-.008Z"
+    />
+  </svg>
+);
+
+export const ShoppingIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    strokeWidth={1.5}
+    stroke="currentColor"
+    className="size-6"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z"
+    />
+  </svg>
+);
