a694b286a4673023919a57b26d2db9472eeafe59
Rendy Saputra <1814138+rensaputra@users.noreply.github.com>
2026-03-31 09:44:25 +1100

    feat: completing add product functionality

--- 20-MyStore-AdminSection/project/admin/src/actions/productActions.ts
+++ 20-MyStore-AdminSection/project/admin/src/actions/productActions.ts
@@ -5,19 +5,20 @@ import { redirect } from "next/navigation";
 import path from "path";
 import fs from "fs";
 import { writeFile } from "fs/promises";
+import { revalidatePath } from "next/cache";
 
 const UPLOAD_DIR = path.resolve("public/uploads");
 
 export async function createProduct(formData: FormData) {
   const data = {
-    name: formData.get("name"),
-    description: formData.get("description"),
-    sellPrice: formData.get("sellPrice"),
-    mrp: formData.get("mrp"),
-    smallSIze: formData.get("smallSize"),
-    mediumSize: formData.get("mediumSize"),
-    largeSize: formData.get("largeSize"),
-    productTypeId: formData.get("productType"),
+    name: formData.get("name") as string,
+    description: formData.get("description") as string,
+    sellPrice: formData.get("sellPrice") as string,
+    mrp: formData.get("mrp") as string,
+    smallSize: formData.get("smallSize") as string,
+    mediumSize: formData.get("mediumSize") as string,
+    largeSize: formData.get("largeSize") as string,
+    productTypeId: formData.get("productType") as string,
     isActive: formData.get("isActive") === "on" ? true : false,
   };
   console.log("Received form data:", data);
@@ -51,4 +52,28 @@ export async function createProduct(formData: FormData) {
     const fullPath = path.join(process.cwd(), "public", imagePath);
     await writeFile(fullPath, buffer);
   }
+
+  const totalStock =
+    parseInt(data.smallSize) +
+    parseInt(data.mediumSize) +
+    parseInt(data.largeSize);
+
+  await db.product.create({
+    data: {
+      name: data.name,
+      description: data.description,
+      sellPrice: parseFloat(data.sellPrice),
+      mrp: parseFloat(data.mrp),
+      smallSize: parseInt(data.smallSize),
+      mediumSize: parseInt(data.mediumSize),
+      largeSize: parseInt(data.largeSize),
+      currentStock: totalStock,
+      productTypeId: Number(data.productTypeId),
+      isActive: data.isActive,
+      image: imagePath,
+    },
+  });
+
+  revalidatePath("/products", "page");
+  redirect("/products");
 }
