44b4af008e8a51363afb81263a29e6d8ff53c626
Rendy Saputra <1814138+rensaputra@users.noreply.github.com>
2026-04-03 18:37:13 +1100

    feat: add category filter

--- 21-MyStore-ClientSection/project/client/src/screens/home/FilterSection.tsx
+++ 21-MyStore-ClientSection/project/client/src/screens/home/FilterSection.tsx
@@ -1,5 +1,47 @@
 const FilterSection = () => {
-  return <div>Filter Section</div>;
+  const CategoryItems = [
+    {
+      label: "All",
+      value: "all",
+    },
+    {
+      label: "Kid's Clothing",
+      value: "Kid's Clothing",
+    },
+    {
+      label: "Men's Clothing",
+      value: "Men's Clothing",
+    },
+  ];
+
+  return (
+    <div className="rounded-lg shadow-lg space-y-3 p-5 bg-white h-fit">
+      <h1 className="text-2xl mb-8 font-semibold">Filters</h1>
+
+      <div className="space-y-2 border-b border-b-gray-300 pb-3">
+        <span className="mb-2 block">Category</span>
+        <div className="flex flex-wrap gap-3 pt-2">
+          {CategoryItems.map((item, index) => {
+            return (
+              <div key={index}>
+                <input
+                  type="checkbox"
+                  id={`productType-${item.value}`}
+                  className="hidden peer"
+                />
+                <label
+                  htmlFor={`productType-${item.value}`}
+                  className="checkbox-button-label"
+                >
+                  {item.label}
+                </label>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default FilterSection;
