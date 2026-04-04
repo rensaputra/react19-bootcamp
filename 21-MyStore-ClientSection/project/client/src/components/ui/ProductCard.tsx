9417993f1300e318df34fc38c1a37ac09dd2bc06
Rendy Saputra <1814138+rensaputra@users.noreply.github.com>
2026-04-04 18:00:35 +1100

    feat: Product card component

---
+++ 21-MyStore-ClientSection/project/client/src/components/ui/ProductCard.tsx
@@ -0,0 +1,47 @@
+import Image from "next/image";
+import { StarIcon } from "../icons";
+
+const ProductCard = () => {
+  return (
+    <div className="bg-white rounded-xl shadow-lg w-full h-full min-h-[624px] px-5">
+      <Image
+        className="w-full h-full rounded-t-xl max-h-96"
+        src="/next.svg"
+        width={0}
+        height={0}
+        sizes="100vw"
+        alt="Product Image"
+      />
+
+      <div className="space-y-4">
+        <div className="space-y-1">
+          <span className="text-2xl font-semibold leading-5">Product Name</span>
+          <p className="text-md text-gray-400 truncate">
+            This is product description.
+          </p>
+        </div>
+      </div>
+
+      <div className="flex justify-between items-center space-y-4">
+        <div className="flex gap-x-3 items-center text-xl font-semibold">
+          <span className="text-gray-500 line-through">$19.99</span>
+          <span className="text-2xl">$16.99</span>
+        </div>
+        <div>
+          <span className="text-gray-400 text-md">12 left</span>
+        </div>
+      </div>
+
+      <div className="flex justify-between items-center">
+        <div className="flex gap-x-1">
+          {[...Array(5)].map((_, index) => {
+            return <StarIcon key={index} />;
+          })}
+        </div>
+        <div className="product-type-label">Product Type</div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductCard;
--- 21-MyStore-ClientSection/project/client/src/screens/home/index.tsx
+++ 21-MyStore-ClientSection/project/client/src/screens/home/index.tsx
@@ -1,5 +1,6 @@
 import { SearchParams } from "next/dist/server/request/search-params";
 import FilterSection from "./FilterSection";
+import ProductCard from "@/components/ui/ProductCard";
 
 const HomeScreen = ({ searchParams }: { searchParams: SearchParams }) => {
   return (
@@ -7,7 +8,9 @@ const HomeScreen = ({ searchParams }: { searchParams: SearchParams }) => {
       <h1 className="text-3xl font-semibold">Products</h1>
       <div className="my-5 grid grid-cols-4 gap-5">
         <FilterSection searchParams={searchParams} />
-        <div className="col-span-3">Product Section</div>
+        <div className="col-span-3 grid grid-cols-2 gap-5">
+          <ProductCard />
+        </div>
       </div>
     </div>
   );
