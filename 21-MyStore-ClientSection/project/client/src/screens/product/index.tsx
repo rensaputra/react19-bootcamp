0bfa3b6741586bd8e617b82946f86f3f036b5132
Rendy Saputra <1814138+rensaputra@users.noreply.github.com>
2026-04-06 17:48:17 +1000

    feat: completed mock UI for product page

--- 21-MyStore-ClientSection/project/client/src/screens/product/index.tsx
+++ 21-MyStore-ClientSection/project/client/src/screens/product/index.tsx
@@ -1,7 +1,14 @@
 import { StarIcon } from "@/components/icons";
+import Button from "@/components/ui/Button";
 import Image from "next/image";
 
 const ProductScreen = ({ productId }: { productId: string }) => {
+  const sizeOptions = [
+    { label: "S", value: "smallSize" },
+    { label: "M", value: "mediumSize" },
+    { label: "L", value: "largeSize" },
+  ];
+
   return (
     <div className="my-10 p-5 rounded-xl bg-white grid grid-cols-2 gap-5">
       <div className="w-full h-full bg-gray-100 rounded-xl p-3">
@@ -32,6 +39,35 @@ const ProductScreen = ({ productId }: { productId: string }) => {
             <span className="line-through">$19.99</span>
           </div>
           <span className="text-gray-500 font-medium">123 item left</span>
+        </div>
+
+        <div className="my-7 space-y-1">
+          <h6 className="text-lg font-semibold">Size</h6>
+          <div className="flex flex-wrap gap-3">
+            {sizeOptions.map((item, index) => (
+              <div key={index}>
+                <input
+                  type="radio"
+                  id={`sizes-${item.value}`}
+                  name="sizes"
+                  className="hidden peer"
+                />
+                <label
+                  htmlFor={`sizes-${item.value}`}
+                  className="checkbox-button-label"
+                >
+                  {item.label}
+                </label>
+              </div>
+            ))}
+          </div>
+        </div>
+        <p className="text-lg font-senibold">Description</p>
+        <p className="text-gray-600">This is the product description.</p>
+
+        <div className="my-7 flex gap-x-5">
+          <Button className="custom-outline-btn w-full">Add to Cart</Button>
+          <Button className="w-full">Buy Now</Button>
         </div>
       </div>
     </div>
