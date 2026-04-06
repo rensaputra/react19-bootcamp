4e2cbc52a3ab10413ae0c36a4928a17663991b4b
Rendy Saputra <1814138+rensaputra@users.noreply.github.com>
2026-04-06 10:11:04 +1000

    feat: implement filter functionality from API level

--- 21-MyStore-ClientSection/project/admin/src/app/api/products/route.ts
+++ 21-MyStore-ClientSection/project/admin/src/app/api/products/route.ts
@@ -2,13 +2,67 @@ import { NextResponse } from "next/server";
 import { db } from "@/lib/db";
 import { ApiResponse, Product } from "@/types";
 
-export async function GET(): Promise<
-  NextResponse<ApiResponse<Product[] | null>>
-> {
+export async function GET(
+  request: Request,
+): Promise<NextResponse<ApiResponse<Product[] | null>>> {
   try {
+    const { searchParams } = new URL(request.url);
+
+    const filters = {
+      productTypeId: searchParams.get("productTypeId")
+        ? parseInt(searchParams.get("productTypeId")!)
+        : undefined,
+      sortBy: searchParams.get("sortBy") || undefined,
+      minPrice: searchParams.get("minPrice")
+        ? Number(searchParams.get("minPrice"))
+        : undefined,
+      maxPrice: searchParams.get("maxPrice")
+        ? Number(searchParams.get("maxPrice"))
+        : undefined,
+      rating: searchParams.get("rating")
+        ? Number(searchParams.get("rating"))
+        : undefined,
+      inStock: searchParams.get("inStock") || undefined,
+    };
+
+    const whereClause = {
+      ...(filters.productTypeId
+        ? { productTypeId: filters.productTypeId }
+        : {}),
+      ...(filters.minPrice || filters.maxPrice
+        ? {
+            sellPrice: {
+              gte: filters.minPrice ?? undefined,
+              lte: filters.maxPrice ?? undefined,
+            },
+          }
+        : {}),
+      ...(filters.rating
+        ? {
+            rating: {
+              gte: filters.rating,
+            },
+          }
+        : {}),
+      ...(filters.inStock === "true"
+        ? { currentStock: { gt: 0 } }
+        : filters.inStock === "false"
+          ? { currentStock: 0 }
+          : {}),
+    };
+
     const products = await db.product.findMany({
       include: {
         productType: true,
+      },
+      where: whereClause,
+      orderBy: {
+        sellPrice:
+          filters.sortBy === "priceLowToHigh"
+            ? "asc"
+            : filters.sortBy === "priceHighToLow"
+              ? "desc"
+              : undefined,
       },
     });
 
