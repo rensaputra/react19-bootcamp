fc05a66d1ed432395da719ee3fb449ecbf87ecfe
Rendy Saputra <1814138+rensaputra@users.noreply.github.com>
2026-04-07 22:57:07 +1000

    feat: completing the mock cart UI

--- 21-MyStore-ClientSection/project/client/src/screens/cart/index.tsx
+++ 21-MyStore-ClientSection/project/client/src/screens/cart/index.tsx
@@ -1,5 +1,10 @@
 import Image from "next/image";
-import { StarIcon, PlusCircleIcon, MinusCircleIcon } from "@/components/icons";
+import {
+  StarIcon,
+  PlusCircleIcon,
+  MinusCircleIcon,
+  TrashIcon,
+} from "@/components/icons";
 import Button from "@/components/ui/Button";
 
 const CartScreen = () => {
@@ -23,15 +28,17 @@ const CartScreen = () => {
               sizes="100vw"
               className="w-60 h-60 object-cover rounded-l-xl m-auto"
             />
-            <div className="flex flex-col p-8">
-              <div className="flex justify-between">
-                <h1 className="text-2xl font-medium">Product Name</h1>
-                <div className="product-type-label">Product Type</div>
-              </div>
-              <div className="flex gap-x-1">
-                {[...Array(5)].map((_, index) => (
-                  <StarIcon key={index} />
-                ))}
+            <div className="flex flex-col p-8 justify-between">
+              <div>
+                <div className="flex justify-between">
+                  <h1 className="text-2xl font-medium">Product Name</h1>
+                  <div className="product-type-label">Product Type</div>
+                </div>
+                <div className="flex gap-x-1">
+                  {[...Array(5)].map((_, index) => (
+                    <StarIcon key={index} />
+                  ))}
+                </div>
               </div>
 
               <div className="text-xl flex gap-x-3 items-center">
@@ -41,42 +48,70 @@ const CartScreen = () => {
                 <span className="text-2xl font-semibold">$14.99</span>
               </div>
 
-              <div className="flex gap-x-4 items-center">
-                <Button className="p-0! bg-transparent! text-black!">
-                  <MinusCircleIcon className="w-8 h-8" />
-                </Button>
-                <span className="text-xl font-semibold">1</span>
-                <Button className="p-0! bg-transparent! text-black!">
-                  <PlusCircleIcon className="w-8 h-8" />
-                </Button>
-              </div>
-
-              <div className="flex gap-x-4 items-center mt-auto">
-                <span>Size:</span>
-                <div className="flex gap-x-2">
-                  {sizeOptions.map((item, index) => (
-                    <div key={index}>
-                      <input
-                        type="radio"
-                        id={`sizes-${item.value}`}
-                        name="sizes"
-                        className="hidden peer"
-                      />
-                      <label
-                        htmlFor={`sizes-${item.value}`}
-                        className="checkbox-button-label"
-                      >
-                        {item.label}
-                      </label>
-                    </div>
-                  ))}
+              <div className="flex items-centers justify-between">
+                <div className="flex gap-x-4 items-center">
+                  <Button className="p-0! bg-transparent! text-black!">
+                    <MinusCircleIcon className="w-8 h-8" />
+                  </Button>
+                  <span className="text-xl font-semibold">1</span>
+                  <Button className="p-0! bg-transparent! text-black!">
+                    <PlusCircleIcon className="w-8 h-8" />
+                  </Button>
+                </div>
+                <div className="flex gap-x-4 items-center mt-auto">
+                  <span>Size:</span>
+                  <div className="flex gap-x-2">
+                    {sizeOptions.map((item, index) => (
+                      <div key={index}>
+                        <input
+                          type="radio"
+                          id={`sizes-${item.value}`}
+                          name="sizes"
+                          className="hidden peer"
+                        />
+                        <label
+                          htmlFor={`sizes-${item.value}`}
+                          className="checkbox-button-label"
+                        >
+                          {item.label}
+                        </label>
+                      </div>
+                    ))}
+                  </div>
+                </div>
+                <div>
+                  <Button className="bg-red-500! w-fit! flex! gap-2! items-center!">
+                    <TrashIcon />
+                    <span>Remove</span>
+                  </Button>
                 </div>
               </div>
             </div>
           </div>
         </div>
 
-        <div>Cart Summary</div>
+        <div className="sticky top-5 h-fit">
+          <div className="flex flex-col justify-between gap-y-5 bg-white shadow-md rounded-xl p-5">
+            <h1 className="border-b font-semibold text-2xl border-gray-400">
+              Cart Summary
+            </h1>
+
+            <div className="space-y-3">
+              <div className="flex justify-between">
+                <span className="truncate">Product Name</span>
+                <span className="text-end">$12.99</span>
+              </div>
+            </div>
+
+            <div className="border-t border-gray-400">
+              <div className="grid grid-cols-2 gap-2 text-xl font-semibold mt-2">
+                <span>Total Amount</span>
+                <span className="text-end">$12.99</span>
+              </div>
+            </div>
+          </div>
+          <Button className="w-full mt-2">Checkout</Button>
+        </div>
       </div>
     </div>
   );
